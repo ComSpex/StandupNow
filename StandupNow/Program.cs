79afe9b86defa15776c2dffa16c2691b4a68a721
Yosuke Maeda <yosuke_maeda@hotmail.com>
2016-12-08 15:12:18 +0900

    Phase 1

--- StandupNow/Program.cs
+++ StandupNow/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
-namespace StandupNow {
-	class Program {
-		static void Main(string[] args) {
-		}
+public class Example {
+	public static void Main() {
+		Example ex = new Example();
+		ex.StartTimer(3000);
+
+		Console.WriteLine("Press Enter to end the program.");
+		Console.ReadLine();
+	}
+
+	public void StartTimer(int dueTime) {
+		Timer t = new Timer(new TimerCallback((state)=> {
+			Timer to = (Timer)state;
+			DateTime now = DateTime.Now;
+			if(TimeHasCome(now)) {
+				Console.WriteLine("{0:O} : MessageBeep will be called.",now);
+			} else {
+				Console.WriteLine("{0:O} : The timer callback executes.",now);
+			}
+		}));
+		t.Change(dueTime,dueTime);
+	}
+	private bool TimeHasCome(DateTime now) {
+		TimeSpan span = new TimeSpan(now.Ticks);
+		return span.Minutes%30==0;
 	}
 }
